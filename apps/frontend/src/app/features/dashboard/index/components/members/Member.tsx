5226e8032ed4df714193fe7e870d07cdb7024dd3
Tomu Maekawa <tomu.m1107@icloud.com>
2025-06-25 17:53:33 +0900

    updated UI of member list

--- apps/frontend/src/app/features/dashboard/index/components/members/Member.tsx
+++ apps/frontend/src/app/features/dashboard/index/components/members/Member.tsx
@@ -3,15 +3,22 @@ import React from "react";
 
 const Member = ({ user }: { user: User }) => {
 	return (
-		<li className="w-full h-10 bg-white shadow-md flex items-center">
+		<li className="w-full h-20 bg-white flex items-center rounded-sm shadow-sm">
 			<div className="flex justify-between px-4 py-3 w-full">
 				<div className="w-1/2 flex items-center gap-3">
-					<img
-						src={user.pictureUrl as string}
-						alt="ユーザー画像"
-						className="w-7 h-7 rounded-full "
-					/>
-					<p className="text-xs font-bold text-right text-black">{user.name}</p>
+					{user.pictureUrl === null ? (
+						<div className="w-7 h-7 rounded-full bg-gray01 " />
+					) : (
+						<img
+							src={user.pictureUrl as string}
+							alt="ユーザー画像"
+							className="w-7 h-7 rounded-full "
+						/>
+					)}
+
+					<p className="text-sm font-bold text-right text-black opacity-80">
+						{user.name}
+					</p>
 				</div>
 				<div className="w-1/2 flex items-center  justify-end gap-5 ">
 					<p
--- apps/frontend/src/app/features/dashboard/index/components/members/MembersContent.tsx
+++ apps/frontend/src/app/features/dashboard/index/components/members/MembersContent.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MembersList from "./MembersList";
 import SearchBar from "./SearchBar";
 
 const MembersContent = () => {
+	const [membersFilter, setMembersFilter] = useState<
+		"all" | "registed" | "notRegist"
+	>("all");
 	return (
-		<div className="w-full h-full">
-			<SearchBar />
-			<MembersList />
+		<div className="w-11/12 h-auto mx-auto mt-10 bg-white shadow-md rounded-sm">
+			<SearchBar
+				membersFilter={membersFilter}
+				setMembersFilter={setMembersFilter}
+			/>
+
+			<MembersList membersFilter={membersFilter} />
 		</div>
 	);
 };
