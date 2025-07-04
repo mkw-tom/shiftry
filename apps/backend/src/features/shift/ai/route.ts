6040261df5be62960f772605eb5d11bd8c212e58
Tomu Maekawa <tomu.m1107@icloud.com>
2025-07-04 11:33:00 +0900

    Fixed the code for not obtaining request headers

--- apps/backend/src/features/shift/ai/route.ts
+++ apps/backend/src/features/shift/ai/route.ts
@@ -4,8 +4,8 @@ import { attachUserId } from "../../../middlewares/request/attachUserId";
 import createShiftController from "./create/controller";
 
 const router = express.Router();
-// router.use(attachUserId);
-// router.use(attachStoreId);
+router.use(attachUserId);
+router.use(attachStoreId);
 
 router.post("/create", createShiftController);
 
