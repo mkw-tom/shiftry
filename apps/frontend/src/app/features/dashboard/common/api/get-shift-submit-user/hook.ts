17c3797404fa7e19a4c9ad2a42d71f98d4476ee4
Tomu Maekawa <tomu.m1107@icloud.com>
2025-07-08 12:41:13 +0900

    Resolve infinite API fetch loop

--- apps/frontend/src/app/features/dashboard/common/api/get-shift-submit-user/hook.ts
+++ apps/frontend/src/app/features/dashboard/common/api/get-shift-submit-user/hook.ts
@@ -7,34 +7,42 @@ import { getSubmittedShiftUser } from "./api";
 export const useGetSubmittedShiftUser = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState<string | null>(null);
-	const { userToken, storeToken } = useSelector(
-		(state: RootState) => state.token,
+
+	const handleGetSubmitShiftUser = useCallback(
+		async ({
+			userToken,
+			storeToken,
+		}: {
+			userToken: string | null;
+			storeToken: string | null;
+		}) => {
+			setIsLoading(true);
+			setError(null);
+
+			try {
+				if (!userToken || !storeToken) {
+					throw new Error(
+						"ユーザートークンまたはストアトークンが見つかりません",
+					);
+				}
+
+				const res = await getSubmittedShiftUser({ userToken, storeToken });
+
+				if (!res.ok) {
+					const message = res.message ?? "通信エラーが発生しました";
+					throw new Error(message);
+				}
+
+				return res;
+			} catch (err) {
+				setError("通信エラーが発生しました");
+				return null;
+			} finally {
+				setIsLoading(false); // どんな場合でもここでOFFになる
+			}
+		},
+		[],
 	);
-
-	const handleGetSubmitShiftUser = useCallback(async () => {
-		setIsLoading(true);
-		setError(null);
-
-		try {
-			if (!userToken || !storeToken) {
-				throw new Error("ユーザートークンまたはストアトークンが見つかりません");
-			}
-
-			const res = await getSubmittedShiftUser({ userToken, storeToken });
-
-			if (!res.ok) {
-				const message = res.message ?? "通信エラーが発生しました";
-				throw new Error(message);
-			}
-
-			return res;
-		} catch (err) {
-			setError("通信エラーが発生しました");
-			return null;
-		} finally {
-			setIsLoading(false); // どんな場合でもここでOFFになる
-		}
-	}, [userToken, storeToken]);
 
 	return { handleGetSubmitShiftUser, isLoading, error };
 };
