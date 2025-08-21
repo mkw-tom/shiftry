e6214abda45122763a5e5cfec6824971a5819df1
Tomu Maekawa <tomu.m1107@icloud.com>
2025-08-21 21:23:34 +0900

    Removed the comment out of the component for liff init

--- apps/frontend/src/app/register/connect/page.tsx
+++ apps/frontend/src/app/register/connect/page.tsx
@@ -1,8 +1,6 @@
 import { liffId } from "@/app/lib/env";
-import AuthGate from "@/shared/components/AuthGate";
 import FirstView from "@/shared/components/FirstView";
 import InitGate from "@/shared/components/InitGate";
-import AuthProvider from "@/shared/context/AuthProvider";
 import React, { Suspense } from "react";
 import ConnectForm from "./components/ConnectForm";
 
@@ -12,17 +10,17 @@ const Page = () => {
 			<div className="w-full md:w-[410px] mx-auto">
 				<FirstView />
 				{/* <AuthGate liffId={liffId.registerConnect}> */}
-				{/* <InitGate liffId={liffId.registerConnect}> */}
-				<Suspense
-					fallback={
-						<div className="w-1/2 mx-auto mt-10">
-							<div className="loading loading-spinner" />
-						</div>
-					}
-				>
-					<ConnectForm />
-				</Suspense>
-				{/* </InitGate> */}
+				<InitGate liffId={liffId.registerConnect}>
+					<Suspense
+						fallback={
+							<div className="w-1/2 mx-auto mt-10">
+								<div className="loading loading-spinner" />
+							</div>
+						}
+					>
+						<ConnectForm />
+					</Suspense>
+				</InitGate>
 				{/* </AuthGate> */}
 			</div>
 		</main>
