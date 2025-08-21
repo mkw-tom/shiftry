2c8405643cc907699caa5602d02497bb7a6e98b1
Tomu Maekawa <tomu.m1107@icloud.com>
2025-08-21 21:16:25 +0900

    Updated API and hooks for store connecting

--- apps/frontend/src/app/register/connect/api/connectLineGroup.ts
+++ apps/frontend/src/app/register/connect/api/connectLineGroup.ts
@@ -8,23 +8,19 @@ import type { StoreConnectLineGroupResponse } from "@shared/api/store/types/conn
 
 export const postConnectLineGroup = async (
 	idToken: string,
-	channelId: string,
-	channelType: "group",
-	storeId: string,
+	groupId: string,
+	storeCode: string,
 ): Promise<
 	StoreConnectLineGroupResponse | ErrorResponse | ValidationErrorResponse
 > => {
 	if (!idToken) {
 		throw new Error("ID Token not found");
 	}
-	if (!storeId) {
-		throw new Error("Store ID not found");
+	if (!groupId) {
+		throw new Error("Group ID not found");
 	}
-	if (!channelType) {
-		throw new Error("Channel Type not found");
-	}
-	if (!channelId) {
-		throw new Error("Channel ID not found");
+	if (!storeCode) {
+		throw new Error("Store code not found");
 	}
 
 	const res = await fetch(`${API_URL}/api/store/connect-line-group`, {
@@ -32,8 +28,8 @@ export const postConnectLineGroup = async (
 		headers: {
 			"Content-Type": "application/json",
 			"x-id-token": idToken,
-			// "x-group-id": groupId,
-			// "x-store-code": storeCode
+			"x-group-id": groupId,
+			"x-store-code": storeCode,
 		},
 	});
 
