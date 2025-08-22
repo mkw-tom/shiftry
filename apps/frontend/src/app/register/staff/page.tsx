d5a21533c6a541c53318ea44d4dc3447f97fcc00
Tomu Maekawa <tomu.m1107@icloud.com>
2025-08-22 18:16:13 +0900

    Removed the comment out of the component for liff init

--- apps/frontend/src/app/register/staff/page.tsx
+++ apps/frontend/src/app/register/staff/page.tsx
@@ -1,5 +1,4 @@
 import { liffId } from "@/app/lib/env";
-// import StaffRegisterContent from "@/app/features/register/staff/components/StaffRegisterContent";
 import InitGate from "@/shared/components/InitGate";
 import FirstView from "../../../shared/components/FirstView";
 import RegisterStaffForm from "./components/ConnectForm";
@@ -9,20 +8,9 @@ const Page = () => {
 		<main className="bg-white w-full h-lvh">
 			<div className="w-full md:w-[410px] mx-auto">
 				<FirstView />
-				{/* <InitGate liffId={liffId.registerStaff}> */}
-				<RegisterStaffForm />
-				{/* </InitGate> */}
-
-				{/* <Suspense
-				{/* <AuthGate liffId={liffId.registerStaff}> */}
-				{/* <InitGate liffId={liffId.registerStaff}> */}
-				{/* <Suspense
-					fallback={
-						<div className="w-1/2 mx-auto mt-10">
-							<div className="loading loading-spinner" />
-						</div>
-					}
-				{/* <StaffRegisterContent /> */}
+				<InitGate liffId={liffId.registerStaff}>
+					<RegisterStaffForm />
+				</InitGate>
 			</div>
 		</main>
 	);
