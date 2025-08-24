6678082c2d9278adb7169691883e7fbd90cd50db
Tomu Maekawa <tomu.m1107@icloud.com>
2025-08-24 15:56:52 +0900

    --no-edit

--- packages/shared/src/api/shift/request/validations/put.ts
+++ packages/shared/src/api/shift/request/validations/put.ts
@@ -2,104 +2,104 @@ import { z } from "zod";
 import { RequestStatus, ShiftType } from "../../../common/types/prisma.js";
 
 const TimeSlot = z.string().regex(/^\d{2}:\d{2}-\d{2}:\d{2}\*\d+$/, {
-	message: "Time must be in format HH:MM-HH:MM*X",
+  message: "Time must be in format HH:MM-HH:MM*X",
 });
 const DefaultTimePositionsSchema = z.object({
-	Monday: z.array(TimeSlot),
-	Tuesday: z.array(TimeSlot),
-	Wednesday: z.array(TimeSlot),
-	Thursday: z.array(TimeSlot),
-	Friday: z.array(TimeSlot),
-	Saturday: z.array(TimeSlot),
-	Sunday: z.array(TimeSlot),
+  Monday: z.array(TimeSlot),
+  Tuesday: z.array(TimeSlot),
+  Wednesday: z.array(TimeSlot),
+  Thursday: z.array(TimeSlot),
+  Friday: z.array(TimeSlot),
+  Saturday: z.array(TimeSlot),
+  Sunday: z.array(TimeSlot),
 });
 export type DefaultTimePositionsType = z.infer<
-	typeof DefaultTimePositionsSchema
+  typeof DefaultTimePositionsSchema
 >;
 
 const OverrideDatesSchema = z.record(
-	z
-		.string()
-		.regex(/^\d{4}-\d{2}-\d{2}$/), // YYYY-MM-DD形式
-	z.array(TimeSlot),
+  z.string().regex(/^\d{4}-\d{2}-\d{2}$/), // YYYY-MM-DD形式
+  z.array(TimeSlot)
 );
 export type OverrideDatesType = z.infer<typeof OverrideDatesSchema>;
 
 export const ShiftsOfRequestsValidate = z.object({
-	defaultTimePositions: DefaultTimePositionsSchema,
-	overrideDates: OverrideDatesSchema,
+  defaultTimePositions: DefaultTimePositionsSchema,
+  overrideDates: OverrideDatesSchema,
 });
 export type ShiftsOfRequestsType = z.infer<typeof ShiftsOfRequestsValidate>;
 
 // ユーザー情報のスキーマ
 export const AbsoluteUserSchema = z.object({
-	userId: z.string(),
-	userName: z.string(),
+  id: z.string(),
+  name: z.string(),
+  pictureUrl: z.string().optional(),
 });
 export const priorityUserSchema = z.object({
-	userId: z.string(),
-	userName: z.string(),
-	level: z
-		.number()
-		.int()
-		.min(1, {
-			message: "Level must be an integer greater than or equal to 1",
-		})
-		.max(3, {
-			message: "Level must be an integer less than or equal to 3",
-		}),
+  id: z.string(),
+  name: z.string(),
+  pictureUrl: z.string().optional(),
+  level: z
+    .number()
+    .int()
+    .min(1, {
+      message: "Level must be an integer greater than or equal to 1",
+    })
+    .max(3, {
+      message: "Level must be an integer less than or equal to 3",
+    }),
 });
 export type AbsoluteUserType = z.infer<typeof AbsoluteUserSchema>;
 
 // 時間帯ごとの割り当て情報スキーマ
 export const TimeSlotSchema = z.object({
-	name: z.string(),
-	count: z
-		.number()
-		.min(1, {
-			message: "Count must be greater than or equal to 1",
-		})
-		.max(3, {
-			message: "Count must be less than or equal to 3",
-		}),
-	absolute: z.array(AbsoluteUserSchema).default([]), // デフォルト空配列
-	priority: z.array(priorityUserSchema).default([]), // デフォルト空配列
-	jobRoles: z.array(z.string()).default([]), // デフォルト空配列
+  name: z.string(),
+  count: z
+    .number()
+    .min(1, {
+      message: "Count must be greater than or equal to 1",
+    })
+    .max(3, {
+      message: "Count must be less than or equal to 3",
+    }),
+  absolute: z.array(AbsoluteUserSchema).default([]), // デフォルト空配列
+  priority: z.array(priorityUserSchema).default([]), // デフォルト空配列
+  jobRoles: z.array(z.string()).default([]), // デフォルト空配列
 });
 export type TimeSlotType = z.infer<typeof TimeSlotSchema>;
 
 // 日付ごとのスキーマ（null も許容）
 export const DateSchema = z.record(
-	z.string(), // 例: "10:00-14:00"
-	TimeSlotSchema,
+  z.string(), // 例: "10:00-14:00"
+  TimeSlotSchema
 );
 export type DateType = z.infer<typeof DateSchema>;
 
 // 全体のスケジュールスキーマ
 export const RequestCalendarValidate = z.record(
-	z.string(), // 例: "2025-08-01"
-	z.union([DateSchema, z.null()]),
+  z.string(), // 例: "2025-08-01"
+  z.union([DateSchema, z.null()])
 );
 
 export type RequestCalendarType = z.infer<typeof RequestCalendarValidate>;
 
 export const upsertShfitRequestValidate = z.object({
-	weekStart: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-		message: "Invalid date format",
-	}),
-	weekEnd: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-		message: "Invalid date format",
-	}),
-	type: z.nativeEnum(ShiftType, {
-		errorMap: () => ({ message: "Invalid shift type" }),
-	}),
-	requests: RequestCalendarValidate,
-	status: z.nativeEnum(RequestStatus, {
-		errorMap: () => ({ message: "Invalid status" }),
-	}),
-	deadline: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-		message: "Invalid date format",
-	}),
+  weekStart: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
+    message: "Invalid date format",
+  }),
+  weekEnd: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
+    message: "Invalid date format",
+  }),
+  type: z.nativeEnum(ShiftType, {
+    errorMap: () => ({ message: "Invalid shift type" }),
+  }),
+  requests: RequestCalendarValidate,
+  status: z.nativeEnum(RequestStatus, {
+    errorMap: () => ({ message: "Invalid status" }),
+  }),
+  deadline: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
+    message: "Invalid date format",
+  }),
 });
 
 export type UpsertShiftRequetType = z.infer<typeof upsertShfitRequestValidate>;
--- packages/shared/src/api/shiftPosition/validations/put-bulk.ts
+++ packages/shared/src/api/shiftPosition/validations/put-bulk.ts
@@ -2,68 +2,80 @@ import { count } from "node:console";
 import z from "zod";
 
 export const WeekDays = z.enum([
-	"monday",
-	"tuesday",
-	"wednesday",
-	"thursday",
-	"friday",
-	"saturday",
-	"sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
 ]);
 export type WeekDayType = z.infer<typeof WeekDays>;
 export type WeekDayArray = WeekDayType[];
 
 export const UpsertShiftPositionValidate = z.object({
-	name: z
-		.string()
-		.min(1, {
-			message: "Name must be a non-empty string",
-		})
-		.max(9, {
-			message: "Name must be at most 9 characters long",
-		}),
-	startTime: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-		message: "Invalid date format",
-	}),
-	endTime: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-		message: "Invalid date format",
-	}),
-	jobRoles: z.array(
-		z.string().min(1, {
-			message: "Job role must be a non-empty string",
-		}),
-	),
-	count: z.number().int().min(1, {
-		message: "Count must be a positive integer",
-	}),
-	weeks: z.array(WeekDays).min(1, {
-		message: "At least one week must be selected",
-	}),
-	absoluteStaff: z
-		.array(
-			z.object({
-				id: z.string().min(1, {
-					message: "Staff ID must be a non-empty string",
-				}),
-				name: z.string().min(1, {
-					message: "Staff name must be a non-empty string",
-				}),
-			}),
-		)
-		.min(1, {
-			message: "At least one absolute staff member must be provided",
-		}),
+  name: z
+    .string()
+    .min(1, {
+      message: "Name must be a non-empty string",
+    })
+    .max(9, {
+      message: "Name must be at most 9 characters long",
+    }),
+  startTime: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
+    message: "Invalid date format",
+  }),
+  endTime: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
+    message: "Invalid date format",
+  }),
+  jobRoles: z.array(
+    z.string().min(1, {
+      message: "Job role must be a non-empty string",
+    })
+  ),
+  count: z.number().int().min(1, {
+    message: "Count must be a positive integer",
+  }),
+  weeks: z.array(WeekDays).min(1, {
+    message: "At least one week must be selected",
+  }),
+  absolute: z
+    .array(
+      z.object({
+        id: z.string().min(1, {
+          message: "Staff ID must be a non-empty string",
+        }),
+        name: z.string().min(1, {
+          message: "Staff name must be a non-empty string",
+        }),
+        pictureUrl: z.string().optional(),
+      })
+    )
+    .min(0),
+  priority: z
+    .array(
+      z.object({
+        id: z.string().min(1, {
+          message: "Staff ID must be a non-empty string",
+        }),
+        name: z.string().min(1, {
+          message: "Staff name must be a non-empty string",
+        }),
+        pictureUrl: z.string().optional(),
+      })
+    )
+    .min(0),
 });
 export type UpsertShiftPositionType = z.infer<
-	typeof UpsertShiftPositionValidate
+  typeof UpsertShiftPositionValidate
 >;
 
 export const bulkUpsertShiftPositionValidate = z
-	.array(UpsertShiftPositionValidate)
-	.min(1, {
-		message: "At least one shift position must be provided",
-	});
+  .array(UpsertShiftPositionValidate)
+  .min(1, {
+    message: "At least one shift position must be provided",
+  });
 
 export type bulkUpsertShiftPositionType = z.infer<
-	typeof bulkUpsertShiftPositionValidate
+  typeof bulkUpsertShiftPositionValidate
 >;
